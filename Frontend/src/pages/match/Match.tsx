f5f9652f50feb7d1449b61063b1a7bb0105b6817
unknown <frederickchandra52@gmail.com>
2024-05-03 15:55:48 +0700

    Update : Complete frontend implementation

--- Frontend/src/pages/match/Match.tsx
+++ Frontend/src/pages/match/Match.tsx
@@ -1,6 +1,7 @@
 import { ScrollView, StyleSheet, Text, View, useColorScheme } from "react-native";
 import useCustomTheme from "../../contexts/ThemeContext";
 import ProfileCards from "./components/ProfileCards";
+import { useState } from "react";
 
 export default function Match() {
     const {colorScheme} = useCustomTheme()
@@ -10,8 +11,8 @@ export default function Match() {
             <View style={styles.page}>
                 <Text style={styles.pageTitle}>Matches</Text>
                 <View style={styles.container}>
-                    <ProfileCards ImageLink={"https://i.pinimg.com/736x/7c/46/9a/7c469af6385d49186e233c27bf091c5f.jpg"}></ProfileCards>
-                    <ProfileCards ImageLink={"https://i.pinimg.com/736x/7c/46/9a/7c469af6385d49186e233c27bf091c5f.jpg"}></ProfileCards>
+                    <ProfileCards ImageLink={"https://i.pinimg.com/736x/7c/46/9a/7c469af6385d49186e233c27bf091c5f.jpg"} Name={"Hanni"} FavoriteState={ true } />
+                    <ProfileCards ImageLink={"https://asianwiki.com/images/4/44/Kim_Min-Ju-2001-p1.jpeg"} Name={"Minju"} FavoriteState={ false }/>
                 </View>
             </View>
         </ScrollView>
--- Frontend/src/pages/match/components/ProfileCards.tsx
+++ Frontend/src/pages/match/components/ProfileCards.tsx
@@ -1,8 +1,9 @@
 import { BlurView } from "@react-native-community/blur";
-import { Dimensions, ImageBackground, StyleSheet, TouchableOpacity } from "react-native";
+import { useState } from "react";
+import { Dimensions, Image, ImageBackground, Platform, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
-export default function MyComponent({ ImageLink , Name, Status}: any) {
-    console.log(ImageLink)
+export default function MyComponent({ ImageLink , Name, FavoriteState}: any) {
+    const [State, setState] = useState(FavoriteState)
     return (
         <TouchableOpacity style={styles.container} activeOpacity={1} onPress={() => {
             // Function Sementara
@@ -11,6 +12,27 @@ export default function MyComponent({ ImageLink , Name, Status}: any) {
             <ImageBackground
                 source={{ uri: ImageLink }}
                 style={styles.card}>
+                <Text style={styles.nameTitle}>{Name}</Text>
+                <View style={styles.buttonContainer}>
+                    <TouchableOpacity style={[styles.toggleContainer, styles.borderRight]} activeOpacity={1} onPress={() => {
+                        console.log('Bro Menolak')
+                    }}>
+                        <Image style={[styles.icon, styles.crossStyle]} source={require('../../../assets/Cross.png')}/>
+                        <BlurView style={styles.blur} blurType="dark" blurAmount={10}/>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={[styles.toggleContainer, styles.borderLeft]} activeOpacity={1} onPress={() => {
+                        if (State === true) {
+                            console.log('Bro Gajadi Favoritekan 💔')
+                            setState(false)
+                        } else if (State === false) {
+                            console.log('Bro Men Favoritekan ❤️')
+                            setState(true)
+                        }
+                    }}>
+                        <Image style={[styles.icon, styles.starStyle, { tintColor: State ? 'yellow' : 'white' }]} source={require('../../../assets/Star.png')}/>
+                        <BlurView style={styles.blur} blurType="dark" blurAmount={10}/>
+                    </TouchableOpacity>
+                </View>
             </ImageBackground>
         </TouchableOpacity>
     )
@@ -24,9 +46,64 @@ const styles = StyleSheet.create({
         overflow: 'hidden',
         borderRadius: screenWidth * 0.055
     },
+    buttonContainer: {
+        width: '100%',
+        height: '23%',
+        overflow: 'hidden',
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+    nameTitle: {
+        fontFamily: 'ABeeZee',
+        fontSize: screenWidth * 0.06,
+        color: 'white',
+        margin: screenWidth * 0.02,
+    },
     card: {
         width: "100%",
         height: "100%",
-        backgroundColor: "red"
+        backgroundColor: "red",
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'flex-end'
+    },
+    blur: {
+        width: '130%',
+        height: '100%',
+        position: 'absolute'
+    },
+    toggleContainer: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '50%',
+        height: '100%',
+        overflow: 'hidden'
+    },
+    borderRight: {
+        borderRightWidth: 1,
+        borderColor: 'white'
+    },
+    borderLeft: {
+        borderLeftWidth: 1,
+        borderColor: 'white'
+    },
+    icon: {
+        position: 'absolute',
+        zIndex: 1
+    },
+    crossStyle: {
+        width: screenWidth * 0.065,
+        height: screenWidth * 0.065,
+        maxWidth: screenWidth * 0.065,
+        maxHeight: screenWidth * 0.065
+    },
+    starStyle: {
+        width: screenWidth * 0.053,
+        height: screenWidth * 0.053,
+        maxWidth: screenWidth * 0.053,
+        maxHeight: screenWidth * 0.053,
     }
 })
