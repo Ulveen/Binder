44b16eff54be37df21ec18be3b4da3d3a2c50914
brynnjoo <joobrynn@gmail.com>
2024-05-27 22:14:16 +0700

    Commit Profile

--- Frontend/src/pages/profile/Profile.tsx
+++ Frontend/src/pages/profile/Profile.tsx
@@ -1,34 +1,76 @@
-import TextButton from "../../components/TextButton";
-import { Image, StyleSheet, Text, View } from "react-native";
+import React, { useEffect } from 'react';
+import { BackHandler, Image, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import TimeService from "../../services/timeService";
 import UserService from "../../services/userService";
 import useAuth from "../../hooks/useAuth";
 import useCustomTheme from "../../hooks/useCustomTheme";
-import CustomTheme from "../../models/CustomTheme";
 import CustomButton from "../../components/CustomButton";
+import CustomTheme from "../../models/CustomTheme";
+import TextHolder from './components/TextHolder';
 
-const userService = UserService()
-const timeService = TimeService()
+interface Props {
+    navigation: any;
+}
 
-export default function Profile() {
-    const { user, logout } = useAuth()
-    const { theme } = useCustomTheme()
+const userService = UserService();
+const timeService = TimeService();
 
-    const styles = getStyles(theme)
-    
+
+
+export default function Profile({ navigation }: Props) {
+    const { user, logout } = useAuth();
+    const { theme } = useCustomTheme();
+
+    const styles = getStyles(theme);
+
+    useEffect(() => {
+        const backAction = () => {
+            navigation.navigate('Home'); // Navigasi kembali ke halaman Home
+            return true; // Kembalikan true untuk menonaktifkan default behavior
+        };
+
+        const backHandler = BackHandler.addEventListener(
+            'hardwareBackPress',
+            backAction
+        );
+
+        return () => backHandler.remove(); // Hapus listener saat komponen tidak lagi digunakan
+    }, [navigation]);
+
+    const handleBackImgPress = () => {
+        navigation.navigate('Home'); // Navigasi kembali ke halaman Home
+    };
+
+    const handleEdit = () => {
+
+    };
+
+    const formattedDob = user?.dob ? new Date(user.dob).toLocaleDateString() : 'Date of Birth not available';
+
+
     return (
         <View style={styles.container}>
+            <TouchableOpacity style={styles.backContainer} onPress={handleBackImgPress}>
+                <Image source={require("../../assets/back.png")} style={styles.backImg}></Image>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.editButton} onPress={() => { /* Handle edit action */ }}>
+                <Text style={styles.editButtonText}>Edit</Text>
+            </TouchableOpacity>
             <Text style={styles.title}>Profile</Text>
             <Image style={styles.profileImage} source={userService.renderProfileImage(user?.profileImage)} />
-            <Text style={styles.profileDetail}>{user?.name}</Text>
-            <Text style={styles.profileDetail}>{timeService.getTimeDiffYear(user?.dob)}</Text>
-            <Text style={styles.profileDetail}>{user?.binusian}</Text>
-            <Text style={styles.profileDetail}>{user?.campus}</Text>
-            <CustomButton onPress={logout}>
-                <Text>Logout</Text>
+            <TextHolder UserInfo={user?.name} TextLabel={"Name"}></TextHolder>
+            <TextHolder UserInfo={formattedDob} TextLabel={"Date of Birth"} />
+            <TextHolder UserInfo={user?.binusian} TextLabel={"Binusian"}></TextHolder>
+            <TextHolder UserInfo={user?.campus} TextLabel={"Campus Area"}></TextHolder>
+            <TextHolder UserInfo TextLabel={"Interested in"}></TextHolder>
+            <CustomButton style={[styles.button]} onPress={logout}>
+                <Text style={[styles.buttonText, { color: 'white' }]}>Logout</Text>
             </CustomButton>
+            <TouchableOpacity style={styles.upgradeButton}>
+                <Text style={styles.upgradeText}>Upgrade to Premium</Text>
+            </TouchableOpacity>
         </View>
-    )
+    );
 }
 
 const getStyles = (theme: CustomTheme) => StyleSheet.create({
@@ -36,22 +78,67 @@ const getStyles = (theme: CustomTheme) => StyleSheet.create({
         flex: 1,
         flexDirection: 'column',
         backgroundColor: theme.background,
-        justifyContent: 'space-evenly',
-        alignItems: 'center'
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        paddingTop: 20,
     },
     title: {
+        marginTop: 30,
         fontSize: 36,
-        fontWeight: 'bold',
-        color: theme.primary
-    },
-    profileDetail: {
-        fontSize: 24,
-        color: theme.text
+        fontStyle: "italic",
+        color: theme.text,
+        fontFamily: "ABeeZee",
+        marginBottom: 15,
     },
     profileImage: {
-        width: 200,
-        height: 200,
-        borderRadius: 100,
-        margin: 10
-    }
-})
+        width: 100,
+        height: 100,
+        borderRadius: 20,
+        marginBottom: 20,
+    },
+    button: {
+        width: '80%',
+        fontSize: 18,
+        color: theme.text,
+        borderColor: '#E8E6EA',
+        borderWidth: 1,
+        padding: 10,
+        paddingVertical: 15,
+        borderRadius: 10,
+        marginVertical: 3,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    buttonText: {
+        color: theme.text,
+        fontWeight: 'bold',
+    },
+    editButton: {
+        position: 'absolute',
+        top: 10,
+        right: 10,
+        padding: 10,
+        backgroundColor: theme.primary,
+        borderColor: '#E8E6EA',
+        borderWidth: 1,
+        borderRadius: 5,
+    },
+    editButtonText: {
+        color: 'white',
+        fontWeight: 'bold',
+    },
+    backContainer: {
+        position: 'absolute',
+        top: 10,
+        left: 10,
+    },
+    backImg: {
+        
+    },
+    upgradeButton: {
+        marginTop: 10,
+    },
+    upgradeText: {
+        fontStyle: "italic",
+    },
+});
---
+++ Frontend/src/pages/profile/components/TextHolder.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Dimensions, StyleSheet, Text, View } from 'react-native';
+import useCustomTheme from "../../../hooks/useCustomTheme";
+import CustomTheme from "../../../models/CustomTheme";
+
+export default function TextHolder({UserInfo, TextLabel}: any){
+    const { theme } = useCustomTheme();
+
+    const styles = getStyles(theme);
+    return (
+        <View style={styles.viewHolder}>
+            <Text style={styles.label}>{TextLabel}</Text>
+            <Text style={styles.profileDetail}>{UserInfo}</Text>
+        </View>
+    )
+}
+
+const screenWidth = Dimensions.get('window').width
+const getStyles = (theme: CustomTheme) => StyleSheet.create({
+    profileDetail: {
+        width: '100%',
+        fontSize: screenWidth * 0.045, // Menggunakan 4.5% dari lebar layar untuk ukuran font
+        color: theme.text,
+        borderColor: '#B0B0B0',
+        borderWidth: 1,
+        padding: screenWidth * 0.025, // Menggunakan 2.5% dari lebar layar untuk padding
+        paddingLeft: screenWidth * 0.037, // Menggunakan 3.7% dari lebar layar untuk padding kiri
+        borderRadius: screenWidth * 0.025, // Menggunakan 2.5% dari lebar layar untuk border radius
+        marginVertical: screenWidth * 0.042, // Menggunakan 4.2% dari lebar layar untuk margin vertikal
+        textAlign: 'left',
+        marginBottom: screenWidth * 0.063, // Menggunakan 6.3% dari lebar layar untuk margin bawah
+    },
+    viewHolder: {
+        width: '80%',
+        alignItems: 'flex-start',
+    },
+    label: {
+        textAlign: 'left',
+        marginBottom: screenWidth *-0.05,
+        paddingLeft: screenWidth *0.017,
+    },
+
+})
